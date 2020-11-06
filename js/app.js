d71e882c29b5b36885e0dabeca388baf5aea9b07
karimTaha <karim.taha@oracle.com>
2020-11-06 14:48:23 +0200

    feat: clicking on a navbar link will scroll to section

--- js/app.js
+++ js/app.js
@@ -19,16 +19,21 @@
 */
 const navbarSections = [{
         "name": "Section One",
-        "id": "section_one"
+        "id": "section_one",
+        "targetId": "section1"
     },
     {
         "name": "Section Two",
-        "id": "section_two"
+        "id": "section_two",
+        "targetId": "section2"
     },
     {
         "name": "Section Three",
-        "id": "section_three"
+        "id": "section_three",
+        "targetId": "section3"
     }];
+
+const navbar = document.querySelector('nav');
 
 /**
  * End Global Variables
@@ -53,18 +58,21 @@ function buildNavbar() {
     for (const section of navbarSections) {
         currentSectionLi = document.createElement('li');
         currentSectionLi.id = section.id;
-        currentSectionLi.textContent = section.name;
         currentSectionLi.className = "menu__link";
+        currentSectionLi.innerHTML = `<a href="#${section.targetId}">${section.name}</a>`;
         fragment.appendChild(currentSectionLi);
     }
     navbarList.appendChild(fragment);
 }
 
 // Add class 'active' to section when near top of viewport
-
+// function addActiveClass()
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToSection(sectionId) {
+    const targetElement = document.querySelector(`${sectionId}`);
+    targetElement.scrollIntoView();
+}
 
 /**
  * End Main Functions
@@ -77,7 +85,13 @@ document.addEventListener('DOMContentLoaded', function () {
     buildNavbar();
 });
 // Scroll to section on link click
-
+navbar.addEventListener('click', function (event) {
+    console.log(event);
+    if (event.target.nodeName === 'A'){
+        event.preventDefault();
+        scrollToSection(event.target.getAttribute("href"));
+    }
+})
 // Set sections as active
 
 
