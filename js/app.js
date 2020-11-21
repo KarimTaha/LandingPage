c193fcf036484919ca457a2662b67d650c26b411
karimTaha <karim.taha@oracle.com>
2020-11-21 17:58:58 +0200

    Changes after review

--- js/app.js
+++ js/app.js
@@ -17,43 +17,22 @@
  * Define Global Variables
  * 
 */
-const navbarSections = [{
-        "name": "Section One",
-        "id": "section_one",
-        "targetId": "section1"
-    },
-    {
-        "name": "Section Two",
-        "id": "section_two",
-        "targetId": "section2"
-    },
-    {
-        "name": "Section Three",
-        "id": "section_three",
-        "targetId": "section3"
-    }];
 
-const navbar = document.querySelector('nav');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function getClosestElement(yPosition) {
-    let resultSectionId;
-    let minDistance = -1;
-    for (const section of navbarSections) {
-        let currentSection = document.querySelector(`#${section.targetId}`);
-        let distance = currentSection.getBoundingClientRect().top;
-        let absDistance = Math.abs(distance);
 
-        if (minDistance == -1 || absDistance < minDistance) {
-            resultSectionId = section.targetId;
-            minDistance = absDistance;
-        }
-    }
-    return resultSectionId;
+function addActiveClass(activeSectionId) {
+    let currentSection = document.querySelector(`#${activeSectionId}`);
+    currentSection.classList.add('your-active-class');
+}
+
+function removeActiveClass(activeSectionId) {
+    let currentSection = document.querySelector(`#${activeSectionId}`);
+    currentSection.classList.remove('your-active-class');
 }
 
 
@@ -65,41 +44,17 @@ function getClosestElement(yPosition) {
 
 // build the nav
 function buildNavbar() {
+    const navbarSections = document.getElementsByTagName('section');
     const navbarList = document.querySelector('#navbar__list');
-    const fragment = document.createDocumentFragment();
-    let currentSectionLi;
 
     for (const section of navbarSections) {
-        currentSectionLi = document.createElement('li');
-        currentSectionLi.id = section.id;
-        currentSectionLi.className = "menu__link";
-        currentSectionLi.innerHTML = `<a href="#${section.targetId}">${section.name}</a>`;
-        fragment.appendChild(currentSectionLi);
+        navbarList.innerHTML += `
+        <li>
+            <a href="#${section.id}" class="menu__link">
+                ${section.getAttribute('data-nav')}
+            </a>
+        </li>`
     }
-    navbarList.appendChild(fragment);
-}
-
-// Add class 'active' to section when near top of viewport
-function addActiveClass(activeSectionId) {
-    for (const section of navbarSections) {
-        let currentSection = document.querySelector(`#${section.targetId}`);
-        if (section.targetId == activeSectionId) {
-            if (!currentSection.classList.contains("your-active-class")){
-                currentSection.classList.add('your-active-class');
-            }
-        }
-        else {
-            if (currentSection.classList.contains("your-active-class")){
-                currentSection.classList.remove('your-active-class');
-            }
-        }
-    }
-}
-
-// Scroll to anchor ID using scrollTO event
-function scrollToSection(sectionId) {
-    const targetElement = document.querySelector(`${sectionId}`);
-    targetElement.scrollIntoView();
 }
 
 /**
@@ -112,17 +67,30 @@ function scrollToSection(sectionId) {
 document.addEventListener('DOMContentLoaded', function () {
     buildNavbar();
 });
-// Scroll to section on link click
-navbar.addEventListener('click', function (event) {
-    if (event.target.nodeName === 'A'){
-        event.preventDefault();
-        scrollToSection(event.target.getAttribute("href"));
-    }
-})
-// Set sections as active
-document.addEventListener('scroll', function () {
-    const yPosition = window.scrollY;
-    const activeSection = getClosestElement(yPosition);
-    addActiveClass(activeSection);
-});
 
+// Set sections as active
+
+// callback function
+const callbackFunction = (entries) => {
+    entries.forEach((entry) => {
+        // only do animation if the element is fully on screen
+        if (entry.isIntersecting) {
+            addActiveClass(entry.target.id)
+        } else {
+            removeActiveClass(entry.target.id)
+        }
+    });
+}
+
+// options for IntersectionObserver
+const options = {
+    threshold: 0.75,
+};
+// Create the IntersectionObserver
+const observer = new IntersectionObserver(callbackFunction, options);
+
+// Observe sections in HTML
+const sections = document.querySelectorAll('section');
+sections.forEach((section) => {
+    observer.observe(section);
+});
